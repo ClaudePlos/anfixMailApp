4efa93152040ddd9fc05152f8c653481127d4b24
ClaudePlos <claude-plos@o2.pl>
2018-08-03 16:44:05 +0200

    v10

--- src/anfixmailapp/AnfixMailApp.java
+++ src/anfixmailapp/AnfixMailApp.java
@@ -6,6 +6,8 @@
 package anfixmailapp;
 
 import anfixmailapp.pl.job.JobCartrackMailRaportHotLead;
+import java.util.HashSet;
+import java.util.Set;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,9 +15,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import org.quartz.CronScheduleBuilder;
+import static org.quartz.DailyTimeIntervalScheduleBuilder.dailyTimeIntervalSchedule;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.TimeOfDay;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
@@ -33,22 +37,29 @@ public class AnfixMailApp extends Application {
         
         Scene scene = new Scene(root);
         //stage.getIcons().add(new Image("/com/images/icon.png"));
-        stage.setTitle("#anfix");
+        stage.setTitle("#anfix Klaudiusz");
         
         stage.setScene(scene);
         stage.show(); 
         
         JobDetail job1 = JobBuilder.newJob(JobCartrackMailRaportHotLead.class).withIdentity("job1", "group1").build();
 
-        /*  CronScheduleBuilder.cronSchedule("0/5 * * * * ?")
-            CronScheduleBuilder.cronSchedule("0/5 * * * * ?")
-            CronScheduleBuilder.dailyAtHourAndMinute(13, 46) //HH:MM
-            CronScheduleBuilder.weeklyOnDayAndHourAndMinute(3, 13, 46)
-            CronScheduleBuilder.monthlyOnDayAndHourAndMinute(28, 13, 46)
+        /*  
+            .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?")) // 5s
+            .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(13, 46)) //HH:MM
+            .withSchedule(CronScheduleBuilder.weeklyOnDayAndHourAndMinute(3, 13, 46))
+            .withSchedule(CronScheduleBuilder.monthlyOnDayAndHourAndMinute(28, 13, 46))
         */
+        Set daysOfWeek = new HashSet();
+        daysOfWeek.add(1);
+        daysOfWeek.add(2);
+        daysOfWeek.add(3);
+        daysOfWeek.add(4);
+        daysOfWeek.add(5);
+        
         Trigger trigger1 = TriggerBuilder.newTrigger()
 					.withIdentity("cronTrigger1", "group1")
-					.withSchedule(CronScheduleBuilder.cronSchedule("0/60 * * * * ?")) //HH:MM
+					.withSchedule(CronScheduleBuilder.cronSchedule("0 55 22 ? * MON-FRI")) //MM HH godz:22:55
 					.build();
 			
         Scheduler scheduler1 = new StdSchedulerFactory().getScheduler();
